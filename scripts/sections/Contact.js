b2212b2f0575a2c44ae1630d6e3141fd29221d77
GriselOrtizC <grisoc930@gmail.com>
2023-10-18 16:13:05 +0200

    Agrego envíio de email desde el form

--- scripts/sections/Contact.js
+++ scripts/sections/Contact.js
@@ -11,79 +11,111 @@ function Contact()  {
   // Crear elementos HTML
   const sectionSix = document.createElement('section')
 
-  const cards = `
-              <div class='cards-six'>
-                <form class='formContact' >
-                    <div class="block">
-                    <h3 class='title-section-two'>CONTACTO</h3>
-                    </div>
-                    <div class="block">
-                            <label for="nombreApellido">Nombre y Apellido:</label>
-                            <input type="text" id="nombreApellido" name="nombreApellido" required>           
-                    </div>
-                    <div >                
-                            <label for="correo">Correo electrónico:</label>
-                            <input type="email" id="correo" name="correo" required>
-                    </div>
-                    <div > 
-                            <label for="celular">Celular:</label>
-                            <input " type="tel" id="celular" name="celular" pattern="[0-9]{10}" required> 
-                    </div>
-                    <div class="block">         
-                            <label for="mensaje">Mensaje:</label>
-                            <textarea id="mensaje" name="mensaje" required></textarea>            
-                    </div>
-                    <div class="blockButton">
-                      <button type="submit" class='button-card-six button-card'>ENVIAR</button>
-                    </div>
-                </form>
-                <div class="container-img-card-small-card-small">       
-                <div class='img-card-small'>
-
-                        <img src =${direccion} class="img-contact"/> 
-                        <a href="mailto:yuliethcardozo@barcelonalogopeda.com"><img src =${email} class="img-contact"/></a> 
-                        <a href="tel:+34678436616"><img src =${telefono} class="img-contact"/></a> 
-
-                </div>
-
-                <div class='card-small' >
-                        <div>
-                        <h4 class= 'tittle-card-Small'>Dirección</h4>       
-                        <h5 class= 'text-card-Small'>C. de Pau Claris, 162, 8-5</h5>
+  const cards = `<div class='cards-six'>
+                        
+                        <form target="_blank" action="https://formsubmit.co/info@barcelonalogopeda.com  " method="POST" class='formContact'>
+                                <div class="block">
+                                        <h3 class='title-section-two'>CONTACTO</h3>
+                                </div>
+                       
+                   
+                                <div  class="block">
+                                        <label for="nombreApellido">Nombre y Apellido:</label>
+                                        <input type="text" name="name" class="form-control" required/>
+                                </div>
+                                <div >
+                                        <label for="correo">Correo electrónico:</label>
+                                        <input type="email" name="email" class="form-control" required/>
+                                </div>
+                                <div >
+                                        <label for="celular">Celular:</label>
+                                        <input type="tel" name="tel" class="form-control" required/>
+                                </div>
+                   
+                        <div  class="block">
+                                <label for="mensaje">Mensaje:</label>
+                                <textarea  class="form-control" name="message" rows="10" required></textarea>
                         </div>
+                        <div class="blockButton">
+                                <button type="submit" class="button-card-six button-card">ENVIAR</button>
+                        </div>
+                        </form>
+                </div>`
 
-                        <img src =${line}  class="img-line"/> 
+//   const cards = `
+        //       <div class='cards-six'>
+        //         <form action="https://formsubmit.co/26e216becfb53b6b4c9174083f43c60a" method="POST" class='formContact'  >
+                    
+        //             <div class="block">
+        //                     <label for="nombreApellido">Nombre y Apellido:</label>
+        //                     <input type="text" name="nombreApellido" id="nombreApellido"  required>           
+        //             </div>
+        //             <div >                
+        //                     <label for="correo">Correo electrónico:</label>
+        //                     <input type="email" name="correo"  id="correo" required>
+        //             </div>
+        //             <div > 
+        //                     <label for="celular">Celular:</label>
+        //                     <input " type="tel" name="celular" id="celular"  pattern="[0-9]{10}" required> 
+        //             </div>
+        //             <div class="block">         
+        //                     <label for="mensaje">Mensaje:</label>
+        //                     <textarea   name="mensaje"   id="mensaje"required></textarea>            
+        //             </div>
+        //             <div class="blockButton">
+        //               <button type="submit" class='button-card-six button-card'>ENVIAR</button>
+        //             </div>
+
+        //             <input type="hidden" name="_next" value="http://127.0.0.1:5500/">
+        //             <input type="hidden" name="_captcha" value="false">
+        //         </form>
+        //         <div class="container-img-card-small-card-small">       
+        //         <div class='img-card-small'>
+
+        //                 <img src =${direccion} class="img-contact"/> 
+        //                 <a href="mailto:info@barcelonalogopeda.com"><img src =${email} class="img-contact"/></a> 
+        //                 <a href="tel:+34678436616"><img src =${telefono} class="img-contact"/></a> 
+
+        //         </div>
+
+        //         <div class='card-small' >
+        //                 <div>
+        //                 <h4 class= 'tittle-card-Small'>Dirección</h4>       
+        //                 <h5 class= 'text-card-Small'>C. de Pau Claris, 162, 8-5</h5>
+        //                 </div>
+
+        //                 <img src =${line}  class="img-line"/> 
                          
-                        <div>
-                        <a href="tel:+34678436616">
-                        <h4 class= 'tittle-card-Small'>Celular</h4>
-                        <h5 class= 'text-card-Small'>(+34) 678 43 66 16</h5>
-                        </a>
-                        </div>
+        //                 <div>
+        //                 <a href="tel:+34678436616">
+        //                 <h4 class= 'tittle-card-Small'>Celular</h4>
+        //                 <h5 class= 'text-card-Small'>(+34) 678 43 66 16</h5>
+        //                 </a>
+        //                 </div>
 
-                        <img src =${line} class="img-line"/> 
+        //                 <img src =${line} class="img-line"/> 
 
-                        <div>
-                        <a href="mailto:yuliethcardozo@barcelonalogopeda.com">
-                        <h4 class= 'tittle-card-Small'>Correo</h4>
-                        <h5 class= 'text-card-Small'>yuliethcardozo@barcelonalogopeda.com</h5>
-                        </a>
-                        </div>
+        //                 <div>
+        //                 <a href="mailto:info@barcelonalogopeda.com">
+        //                 <h4 class= 'tittle-card-Small'>Correo</h4>
+        //                 <h5 class= 'text-card-Small'>info@barcelonalogopeda.com</h5>
+        //                 </a>
+        //                 </div>
 
-                </div>
-                </div>
-              </div>
-              `
+        //         </div>
+        //         </div>
+        //       </div>
+        //       `
 
 const responsiveCards = `
-                        <div class='.responsive-version'>
-                        <form class='formContact' >
+                        <div class='responsive-version'>
+                        <form action="https://formsubmit.co/26e216becfb53b6b4c9174083f43c60a" method="POST" class='formContact' >
                         <div class="block">
                         <h3 class='title-section-two'>CONTACTO</h3>
                         </div>
                         <div class="block">
                                 <label for="nombreApellido">Nombre y Apellido:</label>
-                                <input type="text" id="nombreApellido" name="nombreApellido" required>           
+                                <input type="text" name="nombreApellido" id="nombreApellido"  required>           
                         </div>
                         <div >                
                                 <label for="correo">Correo electrónico:</label>
@@ -91,11 +123,11 @@ const responsiveCards = `
                         </div>
                         <div > 
                                 <label for="celular">Celular:</label>
-                                <input " type="tel" id="celular" name="celular" pattern="[0-9]{10}" required> 
+                                <input " type="tel"  name="celular" id="celular"  pattern="[0-9]{10}" required> 
                         </div>
                         <div class="block">         
                                 <label for="mensaje">Mensaje:</label>
-                                <textarea id="mensaje" name="mensaje" required></textarea>            
+                                <textarea  name="mensaje"   id="mensaje"required></textarea>                        
                         </div>
                         <div class="blockButton">
                                 <button type="submit" class='button-card-six button-card'>ENVIAR</button>
@@ -124,10 +156,10 @@ const responsiveCards = `
                                 <img src =${line} class="img-line"/> 
 
                                 <div>
-                                <a href="mailto:yuliethcardozo@barcelonalogopeda.com">
+                                <a href="mailto:info@barcelonalogopeda.com">
                                 <img src =${email} class="img-contact mail"/> 
                                 <h4 class= 'tittle-card-Small'>Correo</h4>
-                                <h5 class= 'text-card-Small'>yuliethcardozo@barcelonalogopeda.com</h5>
+                                <h5 class= 'text-card-Small'>info@barcelonalogopeda.com</h5>
                                 </a>
                                 </div>
 
@@ -141,7 +173,8 @@ const responsiveCards = `
 
   // Estructurar el DOM
   sectionSix.innerHTML += cards;
-  sectionSix.innerHTML += responsiveCards
+  sectionSix.innerHTML += responsiveCards;
+
 
 
   return sectionSix;
